91ccf14e9e33a0d71287ee85acf785fb261632ff
Hezelnat30 <justin.hezekiel123@gmail.com>
2025-03-06 22:50:11 +0700

    mboh capek

--- src/index.ts
+++ src/index.ts
@@ -6,35 +6,30 @@ import authRouter from "./routes/auth.route";
 import songRoute from "./routes/song.route";
 import mediaRoute from "./routes/media.route";
 
-async function init() {
-  const app = express();
-  app.use(cors());
-  app.use(express.json());
+const app = express();
+app.use(cors());
+app.use(express.json());
 
-  app.get("/", (req, res: Response) => {
-    res.status(200).json({
-      message: "Server is running!",
-      data: null,
-    });
-  });
-
-  const apiRoutes = [authRouter, songRoute, mediaRoute];
-  apiRoutes.forEach((route) => app.use("/api/v1", route));
-
+async function db() {
   try {
-    try {
-      const dbStatus = await connect();
-      console.log("Database status: ", dbStatus);
-    } catch (dbError) {
-      console.error("Database connection error:", dbError);
-    }
-    // Start server
-    app.listen(PORT, () => {
-      console.log(`Server running on port http://localhost:${PORT}`);
-    });
+    const dbStatus = await connect();
+    console.log("Database status: ", dbStatus);
   } catch (error) {
-    console.error("Server initialization error:", error);
+    console.error("Database connection error:", error);
   }
 }
+db();
 
-init();
+app.get("/", (req, res: Response) => {
+  res.status(200).json({
+    message: "Server is running!",
+    data: null,
+  });
+});
+
+const apiRoutes = [authRouter, songRoute, mediaRoute];
+apiRoutes.forEach((route) => app.use("/api/v1", route));
+
+app.listen(PORT, () => {
+  console.log(`Server running on port http://localhost:${PORT}`);
+});
